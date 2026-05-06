5f1864a9fb1cdebd27de72222b724f596103d661
MottSto <timothymumek22@gmail.com>
2026-05-06 13:21:52 +0800

    buat file index

--- Javascript/skor.js
+++ Javascript/skor.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             localStorage.removeItem("sudah_simpan");
             localStorage.removeItem("mulaiBaru");
 
-            window.location.href = "MainMenu.html";
+            window.location.href = "Index.html";
         });
     }
 });
